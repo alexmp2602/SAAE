2239ef51e68e60215e8ac45b8dec77fa3e0a1aa8
alexmp.2602 <alexmp.2602@gmail.com>
2025-07-15 21:31:49 -0300

    feat: Agregado const dynamic en page de importar y aprobaciones para evitar el prerender

--- src/app/importar/page.tsx
+++ src/app/importar/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import ArchivoImportador from "@/components/ArchivoImportador";
 import ComparadorAcciones from "@/components/ComparadorAcciones";
